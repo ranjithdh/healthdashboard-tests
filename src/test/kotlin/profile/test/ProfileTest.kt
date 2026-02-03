749a0e30da7060a277ae8f92d40be6a1dce7078f
Puviarasu Maruthachalam <puviarasu.m@mysmitch.com>
2026-02-03 17:56:17 +0530

    Added Epics

--- src/test/kotlin/profile/test/ProfileTest.kt
+++ src/test/kotlin/profile/test/ProfileTest.kt
@@ -2,18 +2,18 @@ package profile.test
 
 import com.microsoft.playwright.Browser
 import com.microsoft.playwright.BrowserContext
-import com.microsoft.playwright.Page
 import com.microsoft.playwright.Playwright
 import config.BaseTest
 import config.TestConfig
+import io.qameta.allure.Epic
 import login.page.LoginPage
 import model.profile.QuestionerMealType
 import org.junit.jupiter.api.*
-import org.junit.jupiter.api.extension.RegisterExtension
-import utils.screenshots.ScreenshotOnTestCompletionExtension
+import utils.report.Modules
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@Epic(Modules.EPIC_PROFILE)
 class ProfileTest : BaseTest() {
     private lateinit var playwright: Playwright
     private lateinit var browser: Browser
---
+++ src/test/kotlin/utils/report/Epics.kt
@@ -0,0 +1,16 @@
+package utils.report
+
+object Modules {
+    const val MOBILE = "[Mobile]"
+    const val DESKTOP = "[Desktop]"
+
+    const val EPIC_ONBOARDING = "Onboarding $MOBILE"
+    const val EPIC_LOGIN = "Login $MOBILE"
+    const val EPIC_BOOKLABTEST = "Book Lab Test $MOBILE"
+    const val EPIC_CONSULTATIONS = "Consultations $MOBILE"
+    const val EPIC_QUESTIONNAIRE = "Questionnaire $MOBILE"
+    const val EPIC_PROFILE = "Profile $MOBILE"
+
+    const val EPIC_SYMPTOMS = "Symptoms $DESKTOP"
+    const val EPIC_WEBSITE = "Website $DESKTOP"
+}
