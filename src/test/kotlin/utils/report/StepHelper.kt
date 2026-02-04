c3181a64156de672a311e66a8ca0c031c163f63d
Puviarasu Maruthachalam <puviarasu.m@mysmitch.com>
2026-02-04 11:06:12 +0530

    Steps Helper implementation

--- src/test/kotlin/utils/report/StepHelper.kt
+++ src/test/kotlin/utils/report/StepHelper.kt
@@ -8,5 +8,15 @@ object StepHelper {
         Allure.step(name)
     }
 
+    const val ENTER_MOBILE_NUMBER = "Enter mobile number: "
     const val CLEAR_MOBILE_NUMBER = "Clear Mobile Number"
+    const val CLICK_CONTINUE_BUTTON = "Click Continue button"
+    const val SELECT_COUNTRY_CODE = "Select country code: "
+    const val LOGIN_WITH_MOBILE_AND_CONTINUE = "Login with mobile number and continue to OTP"
+    const val TOGGLE_WHATSAPP_CHECKBOX = "Toggle WhatsApp checkbox"
+    const val CLICK_LOGIN_LINK = "Click Login link"
+    const val CLICK_SIGN_UP_LINK = "Click Sign Up link"
+    const val CLICK_PRIVACY_POLICY = "Click Privacy Policy and verify popup"
+    const val CLICK_TERMS_OF_SERVICE = "Click Terms of Service and verify popup"
+
 }
