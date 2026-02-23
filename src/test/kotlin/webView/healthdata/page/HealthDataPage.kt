a8ee082aa0146681fdd67c715fc1062364f6cb77
RanjithSmitch <150891583+RanjithSmitch@users.noreply.github.com>
2026-02-23 19:17:17 +0530

    Slack Notification added.

--- src/test/kotlin/webView/healthdata/page/HealthDataPage.kt
+++ src/test/kotlin/webView/healthdata/page/HealthDataPage.kt
@@ -125,37 +125,4 @@ class HealthDataPage(page: Page, val healthData: HealthData?=null) : BasePage(pa
         trackResult.click()
     }
 
-    fun getHealthDataResponse(): HealthData? {
-        val response = page.waitForResponse(
-            { response: Response? ->
-                response?.url()
-                    ?.contains(TestConfig.APIs.HEALTH_DATA) == true && response.status() == 200
-            },
-            {
-                page.waitForURL(TestConfig.Urls.HOME_PAGE_URL)
-            }
-        )
-
-        val responseBody = response.text()
-        if (responseBody.isNullOrBlank()) {
-            logger.info { "getHealthDataResponse API response body is empty" }
-        }
-
-        try {
-            val responseObj = json.decodeFromString<HealthData>(responseBody)
-
-            if (responseObj.data != null) {
-                StepHelper.step(FETCH_HEALTH_DATA)
-                LogFullApiCall.logFullApiCall(response)
-                return responseObj
-            }
-        } catch (e: Exception) {
-            logger.error { "Failed to parse API response..${e.message}" }
-            return null
-        }
-
-        return null
-    }
-
-
 }
