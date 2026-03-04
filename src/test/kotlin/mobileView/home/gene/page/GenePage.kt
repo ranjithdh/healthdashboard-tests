7a5f5da720ae4a0d448a6389eeb04b564194299e
Puviarasu Maruthachalam <puviarasu.m@mysmitch.com>
2026-03-04 12:23:01 +0530

    Gut Details with Empty conditions

--- src/test/kotlin/mobileView/home/gene/page/GenePage.kt
+++ src/test/kotlin/mobileView/home/gene/page/GenePage.kt
@@ -4,12 +4,37 @@ import com.microsoft.playwright.Page
 import com.microsoft.playwright.options.AriaRole
 import config.BasePage
 import config.TestConfig
-import mobileView.home.gut.page.GutPage
+import mobileView.diagnostics.TestDetailPage
 
-class GenePage(page: Page): BasePage(page)  {
+class GenePage(page: Page) : BasePage(page) {
     override val pageUrl = TestConfig.Urls.BIOMARKERS_URL
 
 
+    fun emptyView() {
+        val dnaHelixImg =
+            page.getByRole(AriaRole.IMG, Page.GetByRoleOptions().setName("DNA helix"))
 
+        val geneticInsightsHeading =
+            page.getByRole(AriaRole.HEADING, Page.GetByRoleOptions().setName("Unlock your Genetic insights"))
+
+        val discoverGenesText =
+            page.getByText("Discover how your genes")
+
+        val geneEmptyStateButton =
+            page.getByTestId("gene-empty-state-view-test-button")
+
+        listOf(
+            dnaHelixImg,
+            geneticInsightsHeading,
+            discoverGenesText,
+            geneEmptyStateButton
+        ).forEach { it.waitFor() }
+
+        geneEmptyStateButton.click()
+
+        TestDetailPage(page)
+            .waitGeneTabLoad()
+            .clickBackButtonToHome()
+    }
 
 }
