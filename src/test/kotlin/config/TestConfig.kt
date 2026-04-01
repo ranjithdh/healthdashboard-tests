4547b5c9de854768934a374a63fecdfe6cecee6e
Puviarasu Maruthachalam <puviarasu.m@mysmitch.com>
2026-04-01 12:39:28 +0530

    Test Config changes

--- src/test/kotlin/config/TestConfig.kt
+++ src/test/kotlin/config/TestConfig.kt
@@ -133,7 +133,6 @@ object TestConfig {
         const val SLOW_MO: Double = (1 * 1000).toDouble()
         const val TIMEOUT: Double = 60000.toDouble()
 
-
         fun launchOptions(): BrowserType.LaunchOptions {
             val isHeadless = System.getenv("HEADLESS")?.toBoolean()
                 ?: System.getProperty("headless")?.toBoolean()
