1c2e7595fc45724e97b144b1541b494c8f602cc1
Puviarasu Maruthachalam <puviarasu.m@mysmitch.com>
2026-04-10 19:07:43 +0530

    checking the CI/CD

--- src/test/kotlin/config/BaseTest.kt
+++ src/test/kotlin/config/BaseTest.kt
@@ -1,13 +1,31 @@
 package config
 
 import com.microsoft.playwright.Page
+import mu.KotlinLogging
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.TestInfo
 import org.junit.jupiter.api.extension.RegisterExtension
+import utils.report.StepHelper
 import utils.screenshots.ScreenshotOnTestCompletionExtension
 
+private val logger = KotlinLogging.logger {}
+
 abstract class BaseTest {
-    // Each test class must override this
     protected lateinit var page: Page
 
     @RegisterExtension
     val screenshotExtension = ScreenshotOnTestCompletionExtension { page }
+
+    @BeforeEach
+    fun beforeEachTest(testInfo: TestInfo) {
+        logger.info { "[SET UP] ${testInfo.displayName}" }
+        StepHelper.step("Test: ${testInfo.displayName}")
+    }
+
+    @AfterEach
+    fun afterEachTest(testInfo: TestInfo) {
+        logger.info { "[TEAR DOWN] ${testInfo.displayName}" }
+        StepHelper.step("Completed: ${testInfo.displayName}")
+    }
 }
