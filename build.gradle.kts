974cebb8d49060fde777fb47e6d08989550bcf9a
SeeniV21 <seenivasan.v@mysmitch.com>
2026-04-16 16:47:52 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,9 @@ dependencies {
 
 
 java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
